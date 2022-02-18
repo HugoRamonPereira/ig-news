42a9da75f91c84c9c3e22204e16384c40b2b9fc1
Ramon Pereira <ramonpereira@MacBook-Pro-Ramon.local>
2022-02-18 00:32:11 -0300

    upload - first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | IG News</title>
+      </Head>
+      <h1>
+        Hello Bitches!
+      </h1>
+    </>
+  );
+}
